e4f13ece133f5555a570fa0e7831dba45adf5661
Kamil Błoński <kblonski02@gmail.com>
2025-05-01 20:25:08 -0800

    Update editor

--- functions.php
+++ functions.php
@@ -1,8 +1,18 @@
 <?php
+/**
+ * 
+ * @package WordPress
+ * @subpackage Twenty_Twenty_Five
+ * @since Twenty Twenty-Five 1.0
+ * 
+ * @author Kamil Błoński
+ */
+
+
 function enqueue_child_theme_styles() {
     wp_enqueue_style(
         'child-style',
-        get_stylesheet_uri(), // ładuje style.css z motywu potomnego
+        get_stylesheet_uri(),
         array(),
         wp_get_theme()->get('Version')
     );
